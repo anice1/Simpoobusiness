24df8caeed62a692b1ce186ed521591d7b583df1
anochima <chimaobinice@gmail.com>
2021-11-30 17:22:30 +0100

    Initial Commit

--- streamlit-app.py
+++ streamlit-app.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 
 """# A prototype for SimpooBusiness item recoginition app"""
-# Draw a title and some text to the app:
+
+# Project Description:
 '''
 The aim of this project is to enable _SimpooBusiness_ identify and validate customer image uploads.
 '''
